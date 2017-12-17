f7ce2a4f768ae2c8a90b54e4d46e77e5a6920e27
tz1006 <a@omg.tf>
2017-12-16 18:42:59 -0800

    Update google.sh

--- google.sh
+++ google.sh
@@ -2,5 +2,11 @@
 
 sudo apt-get -y update
 sudo apt-get -y install nginx
+sudo apt-get -y install letsencrypt
+sudo apt-get -y install python3-pip
+sudo pip3 install setuptools
+sudo pip3 install letsencrypt-nginx
 
 sudo cp -rf /google.conf /etc/nginx/conf.d/google.conf
+
+letsencrypt --nginx -d --email v@omg.tf
